eaff2ef3648f26745efcfe052251d3075d1f1d1b
sichengchen <csc0330@hotmail.com>
2023-11-09 00:22:09 -0500

    Init commit

--- SKCU/BlackScreenSaverView.swift
+++ SKCU/BlackScreenSaverView.swift
@@ -8,11 +8,51 @@
 import SwiftUI
 
 struct BlackScreenSaverView: View {
+    @Binding var isActive: Bool
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        MouseListeningView {
+            // Action to perform when mouse moves
+            self.isActive = false
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(Color.black)
+        .edgesIgnoringSafeArea(.all)
+        .onAppear {
+            NSCursor.hide()
+        }
+        .onDisappear {
+            NSCursor.unhide()
+        }
     }
 }
 
-#Preview {
-    BlackScreenSaverView()
+struct MouseListeningView: NSViewRepresentable {
+    var onMove: () -> Void
+
+    func makeNSView(context: Context) -> NSView {
+        let view = CustomNSView()
+        view.onMove = onMove
+        return view
+    }
+
+    func updateNSView(_ nsView: NSView, context: Context) {}
+}
+
+class CustomNSView: NSView {
+    var onMove: (() -> Void)?
+
+    override func resetCursorRects() {
+        addCursorRect(bounds, cursor: .arrow)
+    }
+
+    override func mouseMoved(with event: NSEvent) {
+        onMove?()
+    }
+
+    override func viewDidMoveToWindow() {
+        guard let window = window else { return }
+        window.acceptsMouseMovedEvents = true
+        window.makeFirstResponder(self)
+    }
 }
--- SKCU/ContentView.swift
+++ SKCU/ContentView.swift
@@ -7,15 +7,74 @@
 
 import SwiftUI
 
+var eventTap: CFMachPort?
+
+func blockKeyboard() {
+    let eventMask = (1 << CGEventType.keyDown.rawValue) | (1 << CGEventType.keyUp.rawValue)
+    eventTap = CGEvent.tapCreate(tap: .cgAnnotatedSessionEventTap,
+                                 place: .headInsertEventTap,
+                                 options: .defaultTap,
+                                 eventsOfInterest: CGEventMask(eventMask),
+                                 callback: { (_, _, _, _) -> Unmanaged<CGEvent>? in return nil },
+                                 userInfo: nil)
+    if let eventTap = eventTap {
+        let runLoopSource = CFMachPortCreateRunLoopSource(kCFAllocatorDefault, eventTap, 0)
+        CFRunLoopAddSource(CFRunLoopGetCurrent(), runLoopSource, .commonModes)
+        CGEvent.tapEnable(tap: eventTap, enable: true)
+    } else {
+        print("Failed to create event tap")
+    }
+}
+
+func releaseKeyboard() {
+    if let eventTap = eventTap {
+        CGEvent.tapEnable(tap: eventTap, enable: false)
+    }
+}
+
 struct ContentView: View {
+    @State var isKeyboardBlocked : Bool = false
+    @State var isFullScreenBlack : Bool = false
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            VStack {
+                Label("Keyboard", systemImage: "keyboard")
+                    .font(.title2)
+                
+                Toggle(isOn: $isKeyboardBlocked, label: {
+                    Text("Lock")
+                })
+                .onChange(of: isKeyboardBlocked, {
+                    if (isFullScreenBlack) {
+                        isFullScreenBlack.toggle()
+                    }
+                    
+                    if (isKeyboardBlocked) {
+                        blockKeyboard()
+                    } else {
+                        releaseKeyboard()
+                    }
+                })
+                .toggleStyle(.switch)
+            }
+            .padding()
+            
+            VStack {
+                Label("Screen", systemImage: "display")
+                    .font(.title2)
+                
+                Button("Black") {
+                    if (isKeyboardBlocked) {
+                        releaseKeyboard()
+                        isKeyboardBlocked.toggle()
+                    }
+                    
+                    isFullScreenBlack.toggle()
+                }
+            }
+            .padding()
         }
-        .padding()
     }
 }
 
