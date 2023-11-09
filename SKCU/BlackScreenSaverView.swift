f38f89cfeeea1f4aa9a6e67a264cc68e7c34ca72
sichengchen <csc0330@hotmail.com>
2023-11-09 00:21:26 -0500

    Init commit

---
+++ SKCU/BlackScreenSaverView.swift
@@ -0,0 +1,18 @@
+//
+//  BlackScreenSaverView.swift
+//  SKCU
+//
+//  Created by Sicheng Chen on 11/8/23.
+//
+
+import SwiftUI
+
+struct BlackScreenSaverView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BlackScreenSaverView()
+}
